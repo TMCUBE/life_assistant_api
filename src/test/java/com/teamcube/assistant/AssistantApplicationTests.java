3212077da2f5c964346af0955289980c2ea21398
GwonDooHyeon <secure3141@gmail.com>
2023-11-30 21:18:07 +0900

    init spring

---
+++ src/test/java/com/teamcube/assistant/AssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamcube.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
